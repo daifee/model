052beb7a67d0a0749242782f59ac831e07e89285
daifee <mr_zhangjiayong@163.com>
2017-11-23 21:25:21 +0800

    完善测试

--- test/Schema.test.js
+++ test/Schema.test.js
@@ -44,7 +44,25 @@ describe.only('lib/Schema', function () {
     expect(schema.get(null, 'not-found', 28)).to.equal(28);
   });
 
-  it('.get(context, attr, originalValue)', function () {
+  it('.get(context, attr, originalValue) 抛出异常', function () {
+    let schema = define({
+      age: {
+        type: NumberType
+      }
+    });
+
+    let err;
+    try {
+      schema.get(null, 'age', NaN);
+    } catch (e) {
+      err = e;
+    }
+
+    expect(err).to.be.an.instanceof(Error);
+    expect(err.name).to.deep.equal('CastError');
+  });
+
+  it('.set(context, attr, originalValue)', function () {
     let schema = define({
       name: {
         type: StringType
@@ -54,6 +72,31 @@ describe.only('lib/Schema', function () {
     expect(schema.set(null, 'name', 'daifee')).to.equal('daifee');
     expect(schema.set(null, 'name', 28)).to.equal(28);
     expect(schema.set(null, 'not-found', 28)).to.equal(28);
+  });
+
+  it('getter & setter', function () {
+    let context = {};
+    let count = 0;
+    let schema = define({
+      name: {
+        type: StringType,
+        get: function (value) {
+          expect(this).to.deep.equal(context);
+          expect(value).to.deep.equal('daifee');
+          count++;
+        },
+        set: function (value) {
+          expect(this).to.deep.equal(context);
+          expect(value).to.deep.equal(28);
+          count++;
+        }
+      }
+    });
+
+    schema.get(context, 'name', 'daifee');
+    schema.set(context, 'name', 28);
+
+    expect(count).to.deep.equal(2);
   });
 
   it('.toJSON(context, originalDoc)', function () {
@@ -74,6 +117,24 @@ describe.only('lib/Schema', function () {
       name: 'daifee',
       age: 28
     });
+  });
+
+  it('.toJSON(context, originalDoc)抛出异常', function () {
+    let schema = define({
+      age: {
+        type: NumberType
+      }
+    });
+
+    let err;
+    try {
+      schema.toJSON(null, {age: NaN});
+    } catch (e) {
+      err = e;
+    }
+
+    expect(err).to.be.an.instanceof(Error);
+    expect(err.name).to.deep.equal('CastError');
   });
 });
 
